2c2329d465128ad7ded6752be089deba48cc9520
nalbcosta <nalbert.schwank@souunit.com.br>
2023-08-07 21:37:02 -0300

    Finished Project

---
+++ index.js
@@ -0,0 +1,26 @@
+const options = {
+    acessibility: true,
+    prevNextButtons: true,
+    pageDots: true,
+    setGallerySize: false,
+    arrowShape: {
+        x0: 1,
+        x1: 58,
+        y1: 62,
+        x2: 55,
+        y2: 48,
+        x3: 18
+    }
+};
+ function setBgPosition(slide, index){
+    const x = -(slide.target + flkty.x) / 3;
+    slides[index].style.backgroundPosition = `${x}px`;
+ };
+
+ const carousel = document.querySelector('[carousel]');
+ const slides = Array.from(document.getElementsByClassName('carousel-cell'));
+ const flkty = new Flickity(carousel, options);
+
+ flkty.on('scroll', ()=>{
+    flkty.slides.forEach(setBgPosition);
+ });
